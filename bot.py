8991a710ee8540a63d7ca9434ec50872008af249
inknos <nicola.sella@outlook.it>
2018-05-09 18:01:28 +0200

    bot buttons

--- bot.py
+++ bot.py
@@ -13,36 +13,60 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-
 import environ
 root = environ.Path(__file__)
 env = environ.Env(DEBUG=(bool, False),) # set default values and casting
 environ.Env.read_env() # reading .env file
 
-# Enable logging
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    keyboard = [[InlineKeyboardButton("Create Event", callback_data='Create Event'),
+                 InlineKeyboardButton("Ask Calendar", callback_data='Ask Calendar')],
 
+                [InlineKeyboardButton("Nothing", callback_data='Nothing')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Main Menu', reply_markup=reply_markup)
+
+def main_menu(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text="{}".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+    
+def create_event_menu(bot, update):
+    keyboard = [[InlineKeyboardButton("", callback_data='Create Event'),
+                 InlineKeyboardButton("Ask Calendar", callback_data='Ask Calendar')],
+
+                [InlineKeyboardButton("Nothing", callback_data='Nothing')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Create Event', reply_markup=reply_markup)
+
+
+def show_calendar_menu(bot, update):
+    keyboard = [[InlineKeyboardButton("Ask for available days", callback_data='Available Days'),
+                 InlineKeyboardButton("Ask Info", callback_data='Info')],
+
+                [InlineKeyboardButton("back", callback_data='back')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Show Calendar', reply_markup=reply_markup)
 
 def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text("Use /start to test this bot.")
 
 
 def error(bot, update, error):
@@ -51,29 +75,19 @@ def error(bot, update, error):
 
 
 def main():
-    """Start the bot."""
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater(env('TOKEN'))
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(env("TOKEN"))
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(main_menu))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
     updater.idle()
 
 
